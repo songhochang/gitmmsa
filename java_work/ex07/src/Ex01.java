386a78da913e4c53e71f94a395135f73e4eba1e8
kdhbd98 <kdhbd98@yu.ac.kr>
2024-07-05 10:06:11 +0900

    0705

---
+++ java_work/ex07/src/Ex01.java
@@ -0,0 +1,13 @@
+public class Ex01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+
+    }
+
+}
